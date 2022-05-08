ca2be706da5c681dbc7b14209225a91474654f3b
razorbladex401 <razorbladex401@gmail.com>
2022-05-07 21:27:08 -0400

    Configure Battleye directory structure

--- docker/entry.sh
+++ docker/entry.sh
@@ -9,10 +9,21 @@ function updateGame() {
         +quit 2>&1 /home/steam/steam.log
 }
 
+function setupBattleye() {
+	if [ ! -f /home/steam/battleye/beserver_x64.dll ] || [ ! -f /home/steam/battleye/beserver_x64.so ];then
+		if [ -f /home/steam/dayz/battleye/beserver_x64.dll ] || [ -f /home/steam/dayz/battleye/beserver_x64.so ];then
+			cd /home/steam/battleye
+			ln -s /home/steam/dayz/battleye/beserver_x64.dll
+			ln -s /home/steam/dayz/battleye/beserver_x64.so
+		fi
+	fi
+}
+
 function startGame() {
 	cd /home/steam/dayz
-	/home/steam/dayz/DayZServer -config="/home/steam/serverDZ.cfg" -adminlog -netlog --dologs --freezeCheck -profiles=/home/steam/profile
+	/home/steam/dayz/DayZServer -config="/home/steam/serverDZ.cfg" -adminlog -netlog --dologs --freezeCheck -profiles=/home/steam/profile -BEpath=/home/steam/battleye
 }
 
 updateGame
+setupBattleye
 startGame
